bd01232767f2a1da4184556bdc5b912716f4a370
James Mace <macewjames@gmail.com>
2025-11-15 09:47:18 -0800

    added all necessary dependencies and set up CMakeLists.txt

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "finally working" << std::endl;
+    return 0;
+}
